d769c8f38ca1f16c9668191444b9cc3e6f035116
Albert Bertram <bertrama@umich.edu>
2017-05-23 13:19:38 -0400

    Adapt the UW-Madison oEmbed plugin for Kaltura for our site.

---
+++ wp-content/plugins/umlib_kaltura_oembed.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Plugin Name: UM-Lib Kaltura Enable oEmbed
+	Plugin URI:
+	Description: Enable UM-Library Kaltura oEmbed within your site.
+	Version: 1.0
+	Author: Tony Box
+	License: GPL2
+*/
+add_action( 'init', 'umlib_kaltura_add_oembed_handlers');
+function umlib_kaltura_add_oembed_handlers() {
+    wp_oembed_add_provider( 'https://lib.mivideo.it.umich.edu/id/*', 'https://lib.mivideo.it.umich.edu/oembed/', false );
+}
