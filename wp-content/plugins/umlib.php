f4972dab9d11de2e3b3b626a24404394fa3212a3
Albert Bertram <bertrama@umich.edu>
2017-05-25 10:55:14 -0400

    Move the umask and Kaltura video oembed into a single plugin.

---
+++ wp-content/plugins/umlib.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	Plugin Name: U-M Library setup.
+	Plugin URI: https://github.com/mlibrary/microsites
+	Description: Provide some basic setup for U-M Library sites.
+	Version: 1.0
+	Author: Albert Bertram
+	License: BSD-Revised
+*/
+add_action( 'init', 'umlib_setup');
+function umlib_setup() {
+    umask(002);
+    wp_oembed_add_provider(
+      'https://lib.mivideo.it.umich.edu/*',
+      'https://lib.mivideo.it.umich.edu/oembed',
+       false
+    );
+}
