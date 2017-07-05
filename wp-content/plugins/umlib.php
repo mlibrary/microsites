4be982ec97f6016c19364c8631edd707b4b855dc
Albert Bertram <bertrama@umich.edu>
2017-07-05 14:36:31 -0400

    Make adding friend accounts possible.

--- wp-content/plugins/umlib.php
+++ wp-content/plugins/umlib.php
@@ -7,7 +7,9 @@
 	Author: Albert Bertram
 	License: BSD-Revised
 */
+
 add_action( 'init', 'umlib_setup');
+
 function umlib_setup() {
     umask(002);
     wp_oembed_add_provider(
@@ -16,3 +18,12 @@ function umlib_setup() {
        false
     );
 }
+
+// With hints from https://stackoverflow.com/questions/8916631/make-wordpress-user-name-the-email-address
+add_filter('wpmu_validate_user_signup', function($info) {
+  $_POST['noconfirmation'] = 1;
+  if (isset($info['errors']->errors['user_name'])) {
+    unset($info['errors']->errors['user_name']);
+  }
+  return $info;
+});
