5402cbac6aa3661bbb0e323cd225e2481676cc37
Albert Bertram <bertrama@umich.edu>
2017-09-19 01:24:28 -0400

    Add a GH gist shortcode and syntax highlihter.

--- wp-content/plugins/umlib.php
+++ wp-content/plugins/umlib.php
@@ -5,7 +5,7 @@
 	Description: Provide some basic setup for U-M Library sites.
 	Version: 1.0
 	Author: Albert Bertram
-	License: BSD-Revised
+	License: BSD-Revised, GPL as required.
 */
 
 add_action( 'init', 'umlib_setup');
@@ -27,3 +27,35 @@ add_filter('wpmu_validate_user_signup', function($info) {
   }
   return $info;
 });
+
+// Some portions of this are modeled after the WP pastacode plugin
+// The author of that plugin is Willy Bahuaud.
+function umlib_gist_shortcode($atts) {
+
+  $id   = isset($atts['id']) ? $atts['id'] :
+          isset($atts[0]) ? explode('/', $atts[0])[4] :
+          NULL;
+
+  $file = isset($atts['file']) ? $atts['file'] : NULL;
+
+  if (!empty($id)) {
+    $req  = wp_sprintf('https://api.github.com/gists/%s', $id);
+    $code = wp_remote_get($req);
+    if (!is_wp_error($code) && 200 == wp_remote_retrieve_response_code($code)) {
+      $data = json_decode(wp_remote_retrieve_body($code));
+      $url  = $data->html_url;
+      if ($file && isset($data->files->{$file})) {
+        $data = $data->files->{$file};
+      }
+      else {
+        $data = (array) $data->files;
+        $data = reset($data);
+      }
+      $name = esc_html($data->filename);
+      $code = esc_html($data->content);
+      $raw  = esc_html($data->raw_url);
+    }
+  }
+  return "<div><pre>{$code}</pre><a href=\"{$url}\">GitHub Gist: {$name}</a></div>";
+}
+add_shortcode('gist', 'umlib_gist_shortcode');
