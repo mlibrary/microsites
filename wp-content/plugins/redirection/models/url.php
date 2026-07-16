830ca26189ff80e2a4a36d7fd3f5072b49e61886
Albert Bertram <bertrama@umich.edu>
2026-07-16 17:12:47 -0400

    Update redirection.

--- wp-content/plugins/redirection/models/url.php
+++ wp-content/plugins/redirection/models/url.php
@@ -1,9 +1,10 @@
 <?php
 
-include_once dirname( __FILE__ ) . '/url-query.php';
-include_once dirname( __FILE__ ) . '/url-path.php';
-include_once dirname( __FILE__ ) . '/url-match.php';
-include_once dirname( __FILE__ ) . '/url-flags.php';
+require_once __DIR__ . '/url-query.php';
+require_once __DIR__ . '/url-path.php';
+require_once __DIR__ . '/url-match.php';
+require_once __DIR__ . '/url-flags.php';
+require_once __DIR__ . '/url-request.php';
 
 class Red_Url {
 	private $url;
@@ -37,7 +38,7 @@ class Red_Url {
 		}
 
 		$path = new Red_Url_Path( $this->url );
-		$query = new Red_Url_Query( $this->url );
+		$query = new Red_Url_Query( $this->url, $flags );
 
 		return $path->is_match( $requested_url, $flags ) && $query->is_match( $requested_url, $flags );
 	}
--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.2.3' );
-define( 'REDIRECTION_BUILD', '832f7155834bcdc64325009360c6273b' );
-define( 'REDIRECTION_MIN_WP', '4.5' );
+define( 'REDIRECTION_VERSION', '4.8' );
+define( 'REDIRECTION_BUILD', '7acce5951ed37d0a438ffb92e849136e' );
+define( 'REDIRECTION_MIN_WP', '4.6' );
